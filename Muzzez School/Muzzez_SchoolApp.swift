9ff1fda25e2956b87c00134d02fc9112b2f95c88
gusadi09 <gusadi872@gmail.com>
2021-04-28 12:50:27 +0800

    Initial Commit

---
+++ Muzzez School/Muzzez_SchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muzzez_SchoolApp.swift
+//  Muzzez School
+//
+//  Created by Gus Adi on 28/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Muzzez_SchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
